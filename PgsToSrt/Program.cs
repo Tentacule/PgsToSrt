47a36184f3cb4b8e27917ee3e99470c1c25c2313
Tentacule <373441+tentacule@users.noreply.github.com>
2019-01-10 22:10:24 +0100

    fixed typo + build in release configuration

--- PgsToSrt/Program.cs
+++ PgsToSrt/Program.cs
@@ -13,27 +13,27 @@ namespace PgsToSrt
         static void Main(string[] args)
         {
             var options = Parser.Default.ParseArguments<CommandLineOptions>(args);
-            var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
-            Console.WriteLine($"PgsToSrt {version}");
-            Console.WriteLine();
+            if (options is Parsed<CommandLineOptions> values)
+            {
+                var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                Console.WriteLine($"PgsToSrt {version}");
+                Console.WriteLine();
 
-            var servicesProvider = new ServiceCollection()
-               .AddLogging(builder =>
-               {
-                   builder.SetMinimumLevel(LogLevel.Trace);
-                   builder.AddNLog();
-               })
-               .AddTransient<Runner>()
-               .BuildServiceProvider();
+                var servicesProvider = new ServiceCollection()
+                    .AddLogging(builder =>
+                    {
+                        builder.SetMinimumLevel(LogLevel.Trace);
+                        builder.AddNLog();
+                    })
+                    .AddTransient<Runner>()
+                    .BuildServiceProvider();
 
-            var runner = servicesProvider.GetRequiredService<Runner>();
-            var values = options as Parsed<CommandLineOptions>;
+                var runner = servicesProvider.GetRequiredService<Runner>();
 
-            if (runner.Run(values))
-                Console.Write("Done.");
-
-            Console.ReadLine();
+                if (runner.Run(values))
+                    Console.Write("Done.");
+            }
         }
     }
 }
