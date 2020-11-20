c3e2cda532c1a1c20fdebf084558ef371431cf82
Tentacule <git@tentacule.net>
2020-11-20 23:41:09 +0100

    Tesseract api access refactoring

--- PgsToSrt/TesseractApi.cs
+++ PgsToSrt/TesseractApi.cs
@@ -21,69 +21,53 @@ namespace PgsToSrt
             var tessApiCustomType = CreateInterfaceType<ITessApiSignatures>("tesseract41", "tesseract", "4");
             var leptApiCustomType = CreateInterfaceType<ILeptonicaApiSignatures>("leptonica-1.80.0", "lept", "5");
 
-
             var loader = new NativeLoader();
             loader.WindowsOptions.UseSetDllDirectory = true;
 
-            var tessApiNative = (ITessApiSignatures)loader.CreateInstance(tessApiCustomType);
-            var leptApiNative = (ILeptonicaApiSignatures)loader.CreateInstance(leptApiCustomType);
+            var tessApiInstance = (ITessApiSignatures)loader.CreateInstance(tessApiCustomType);
+            var leptApiInstance = (ILeptonicaApiSignatures)loader.CreateInstance(leptApiCustomType);
 
-            tessApiType.GetField("native", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, tessApiNative);
-            leptApiType.GetField("native", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, leptApiNative);
+            tessApiType.GetField("native", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, tessApiInstance);
+            leptApiType.GetField("native", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, leptApiInstance);
         }
 
         public static Type CreateInterfaceType<T>(string windowsLibraryName, string commonLibraryName, string version)
         {
-            var type = typeof(T);
-            var infos = GetInfo(type);
-            var typeName = $"{_assemblyName}.{type.Name}2";
-
+            var interfaceType = typeof(T);
+            var typeName = $"{_assemblyName}.{interfaceType.Name}2";
             var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(_assemblyName), AssemblyBuilderAccess.Run);
             var moduleBuilder = assemblyBuilder.DefineDynamicModule(_assemblyName);
-            var typeBuilder = moduleBuilder.DefineType(typeName, TypeAttributes.Interface | TypeAttributes.Abstract | TypeAttributes.Public, null, new Type[1] { type });
+            var typeBuilder = moduleBuilder.DefineType(typeName, TypeAttributes.Interface | TypeAttributes.Abstract | TypeAttributes.Public, null, new Type[1] { interfaceType });
+            var tesseractMethods = GetTesseractMethods(interfaceType);
 
-            foreach (var item in infos)
+            foreach (var tesseractMethod in tesseractMethods)
             {
-                var methodInfo = item.Key;
+                var methodInfo = tesseractMethod.Key;
                 var parametersType = from p in methodInfo.GetParameters() select p.ParameterType;
 
                 var methodBuilder = typeBuilder.DefineMethod(
                     methodInfo.Name, methodInfo.Attributes,
                     methodInfo.CallingConvention, methodInfo.ReturnType, parametersType.ToArray());
 
-                // NativeLoaderOverrideAttribute
-                var nativeLoaderOverrideAttributeType = typeof(NativeLoaderOverrideAttribute);
-                var nativeLoaderOverrideAttributeConstructorInfo = nativeLoaderOverrideAttributeType.GetConstructor(new Type[0] { });
-                var nativeLoaderOverrideAttributePpropNameValues = new Dictionary<string, object>();
+                var nativeLoaderOverrideAttributeProperties = new Dictionary<string, object>();
+                nativeLoaderOverrideAttributeProperties.Add("LibraryName", windowsLibraryName);
+                nativeLoaderOverrideAttributeProperties.Add("Platform", Platform.Windows);
+                methodBuilder.AddCustomAttribute<NativeLoaderOverrideAttribute>(nativeLoaderOverrideAttributeProperties);
 
-                nativeLoaderOverrideAttributePpropNameValues.Add("LibraryName", windowsLibraryName);
-                nativeLoaderOverrideAttributePpropNameValues.Add("Platform", Platform.Windows);
-
-                var (propertyInfos, propertyValues) = GetProperties(nativeLoaderOverrideAttributeType, nativeLoaderOverrideAttributePpropNameValues);
-                var attributeBuilder = new CustomAttributeBuilder(nativeLoaderOverrideAttributeConstructorInfo, new object[0] { }, propertyInfos, propertyValues);
-                methodBuilder.SetCustomAttribute(attributeBuilder);
-
-                // RuntimeUnmanagedFunctionPointerAttribute
-                var runtimeUnmanagedFunctionPointerAttributeType = typeof(RuntimeUnmanagedFunctionPointerAttribute);
-                var runtimeUnmanagedFunctionPointerAttributeConstructorInfo = runtimeUnmanagedFunctionPointerAttributeType.GetConstructor(new Type[0] { });
-                var runtimeUnmanagedFunctionPointerAttributePropNameValues = new Dictionary<string, object>();
-
-                runtimeUnmanagedFunctionPointerAttributePropNameValues.Add("LibraryName", commonLibraryName);
-                runtimeUnmanagedFunctionPointerAttributePropNameValues.Add("LibraryVersion", version);
-                runtimeUnmanagedFunctionPointerAttributePropNameValues.Add("CallingConvention", CallingConvention.Cdecl);
-                runtimeUnmanagedFunctionPointerAttributePropNameValues.Add("EntryPoint", item.Value["EntryPoint"]);
-
-                var (propertyInfos2, propertyValues2) = GetProperties(runtimeUnmanagedFunctionPointerAttributeType, runtimeUnmanagedFunctionPointerAttributePropNameValues);
-                var attributeBuilder2 = new CustomAttributeBuilder(runtimeUnmanagedFunctionPointerAttributeConstructorInfo, new object[0] { }, propertyInfos2, propertyValues2);
-                methodBuilder.SetCustomAttribute(attributeBuilder2);
+                var runtimeUnmanagedFunctionPointerAttributeProperties = new Dictionary<string, object>();
+                runtimeUnmanagedFunctionPointerAttributeProperties.Add("LibraryName", commonLibraryName);
+                runtimeUnmanagedFunctionPointerAttributeProperties.Add("LibraryVersion", version);
+                runtimeUnmanagedFunctionPointerAttributeProperties.Add("CallingConvention", CallingConvention.Cdecl);
+                runtimeUnmanagedFunctionPointerAttributeProperties.Add("EntryPoint", tesseractMethod.Value);
+                methodBuilder.AddCustomAttribute<RuntimeUnmanagedFunctionPointerAttribute>(runtimeUnmanagedFunctionPointerAttributeProperties);
             }
 
             return typeBuilder.CreateType();
         }
 
-        public static List<KeyValuePair<MethodInfo, Dictionary<string, object>>> GetInfo(Type type)
+        public static List<KeyValuePair<MethodInfo, string>> GetTesseractMethods(Type type)
         {
-            var result = new List<KeyValuePair<MethodInfo, Dictionary<string, object>>>();
+            var result = new List<KeyValuePair<MethodInfo, string>>();
             var runtimeDllImportAttributeType = typeof(Tesseract.Page).Assembly.GetType("InteropDotNet.RuntimeDllImportAttribute");
 
             foreach (var methodInfo in type.GetMethods())
@@ -92,41 +76,40 @@ namespace PgsToSrt
 
                 if (runtimeDllImportAttribute != null)
                 {
-                    var dict = new Dictionary<string, object>();
-
-                    foreach (var propertyInfo in runtimeDllImportAttribute.GetType().GetProperties())
-                    {
-                        dict.Add(propertyInfo.Name, propertyInfo.GetValue(runtimeDllImportAttribute));
-                    }
-
-                    foreach (var fieldInfo in runtimeDllImportAttribute.GetType().GetFields())
-                    {
-                        dict.Add(fieldInfo.Name, fieldInfo.GetValue(runtimeDllImportAttribute));
-                    }
-
-                    result.Add(new KeyValuePair<MethodInfo, Dictionary<string, object>>(methodInfo, dict));
+                    var entryPoint = (string)runtimeDllImportAttribute.GetType().GetField("EntryPoint").GetValue(runtimeDllImportAttribute);
+                    result.Add(new KeyValuePair<MethodInfo, string>(methodInfo, entryPoint));
                 }
             }
 
             return result;
         }
 
-        public static (PropertyInfo[] propertyInfos, object[] propertyValues) GetProperties(Type type, Dictionary<string, object> propertyNameValues)
+        public static void AddCustomAttribute<T>(this MethodBuilder methodBuilder, Dictionary<string, object> propertiesNameValue)
         {
-            var p1 = new List<PropertyInfo>();
-            var p2 = new List<object>();
+            var attributeType = typeof(T);
+            var attributeConstructorInfo = attributeType.GetConstructor(new Type[0] { });
+            var (propertyInfos, propertyValues) = SplitPropertiesNameValue(attributeType, propertiesNameValue);
+            var attributeBuilder = new CustomAttributeBuilder(attributeConstructorInfo, new object[0] { }, propertyInfos, propertyValues);
 
-            foreach (var item in propertyNameValues)
+            methodBuilder.SetCustomAttribute(attributeBuilder);
+        }
+
+        public static (PropertyInfo[] propertyInfos, object[] propertyValues) SplitPropertiesNameValue(Type type, Dictionary<string, object> propertiesNameValue)
+        {
+            var propertyInfos = new List<PropertyInfo>();
+            var propertyValues = new List<object>();
+
+            foreach (var item in propertiesNameValue)
             {
                 var propertyName = item.Key;
                 var propertyValue = item.Value;
 
                 var property = type.GetProperty(propertyName);
-                p1.Add(property);
-                p2.Add(propertyValue);
+                propertyInfos.Add(property);
+                propertyValues.Add(propertyValue);
             }
 
-            return (p1.ToArray(), p2.ToArray());
+            return (propertyInfos.ToArray(), propertyValues.ToArray());
         }
 
     }
