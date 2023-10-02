d958b42b54b6b4df930b80b94dbf121a73440257
Tentacule <git@tentacule.net>
2023-10-02 22:18:54 +0200

    updated publish script

--- src/publish.sh
+++ src/publish.sh
@@ -1,2 +1,10 @@
-dotnet publish -f net5.0 --no-self-contained -o out/net5
-dotnet publish -f net6.0 --no-self-contained -o out/net6
+VERSION="1.4.4"
+
+sed -i "s/0.0.0.0/$VERSION.0/" ./PgsToSrt/PgsToSrt.csproj
+sed -i "s/0.0.0/$VERSION/" ./PgsToSrt/PgsToSrt.csproj
+
+dotnet publish -f net6.0 --no-self-contained -o out/publish
+
+cd out/publish
+zip -r ../PgsToStr-$VERSION.zip ./*
+cd ..
