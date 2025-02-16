bf1f18c0c681fc353d6561d2049f23b3510c915b
Courtland <Courtland9777@gmail.com>
2025-02-16 03:46:46 -0500

    Updated net6.0 to net8.0

--- src/publish.sh
+++ src/publish.sh
@@ -3,7 +3,7 @@ VERSION="1.4.6"
 sed -i "s/0.0.0.0/$VERSION.0/" ./PgsToSrt/PgsToSrt.csproj
 sed -i "s/0.0.0/$VERSION/" ./PgsToSrt/PgsToSrt.csproj
 
-dotnet publish -f net6.0 --no-self-contained -o out/publish
+dotnet publish -f net8.0 --no-self-contained -o out/publish
 
 cd out/publish
 zip -r ../PgsToStr-$VERSION.zip ./*
